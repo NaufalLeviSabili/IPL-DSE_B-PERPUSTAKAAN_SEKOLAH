37f352da90c8307d1fad28ace8fcbf3881035dad
Afuzaaaa <114900946+Afuzaaaa@users.noreply.github.com>
2022-12-05 11:28:50 +0700

    Menambahkan id_anggota, id_buku, no_petugas
    menambahkan id_anggota, id_buku, no_petugas dalam tabel peminjaman

--- perpustakaan_sma.sql
+++ perpustakaan_sma.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 100408
 File Encoding         : 65001
 
-Date: 2022-12-05 00:45:45
+Date: 2022-12-05 01:08:11
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -57,6 +57,9 @@ CREATE TABLE `peminjaman` (
   `id_peminjaman` varchar(20) NOT NULL,
   `tgl_pinjam` date DEFAULT NULL,
   `tgl_kembali` date DEFAULT NULL,
+  `id_anggota` varchar(20) DEFAULT NULL,
+  `id_buku` varchar(20) DEFAULT NULL,
+  `no_petugas` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`id_peminjaman`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
