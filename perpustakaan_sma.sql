5437189ebe0d526fdf6cc7bec7d1cbd9f7c4d920
Afuzaaaa <114900946+Afuzaaaa@users.noreply.github.com>
2022-12-05 11:31:25 +0700

    Update tipe data tabel peminjaman dan foreign key
    Merubah tipe data id_anggota, id_buku, no_petugas, dan id_peminjaman menjadi int, dan menambahkan foreign key dalam kolom id_anggota, id_buku, dan no_petugas, dan menambahkan relasi dari masing masing tabel agar saling terhubung

--- perpustakaan_sma.sql
+++ perpustakaan_sma.sql
@@ -1,85 +1,150 @@
-/*
-Navicat MySQL Data Transfer
+-- phpMyAdmin SQL Dump
+-- version 4.9.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 05 Des 2022 pada 05.29
+-- Versi server: 10.4.8-MariaDB
+-- Versi PHP: 7.3.10
 
-Source Server         : Zoey
-Source Server Version : 100408
-Source Host           : localhost:3306
-Source Database       : perpustakaan_sma
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-Target Server Type    : MYSQL
-Target Server Version : 100408
-File Encoding         : 65001
 
-Date: 2022-12-05 01:08:11
-*/
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-SET FOREIGN_KEY_CHECKS=0;
+--
+-- Database: `perpustakaan_sma`
+--
 
--- ----------------------------
--- Table structure for anggota
--- ----------------------------
-DROP TABLE IF EXISTS `anggota`;
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `anggota`
+--
+
 CREATE TABLE `anggota` (
   `id_anggota` int(20) NOT NULL,
   `nama` char(20) DEFAULT NULL,
   `jenis_kelamin` varchar(20) DEFAULT NULL,
-  `no_telp` varchar(20) DEFAULT NULL,
-  PRIMARY KEY (`id_anggota`)
+  `no_telp` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- ----------------------------
--- Records of anggota
--- ----------------------------
+-- --------------------------------------------------------
 
--- ----------------------------
--- Table structure for buku
--- ----------------------------
-DROP TABLE IF EXISTS `buku`;
+--
+-- Struktur dari tabel `buku`
+--
+
 CREATE TABLE `buku` (
-  `id_buku` int(20) NOT NULL AUTO_INCREMENT,
+  `id_buku` int(20) NOT NULL,
   `judul_buku` varchar(20) NOT NULL,
   `kategori_buku` varchar(20) NOT NULL,
   `penerbit_buku` varchar(20) NOT NULL,
   `pengarang_buku` varchar(20) NOT NULL,
-  `tahun_buku` date NOT NULL,
-  PRIMARY KEY (`id_buku`)
+  `tahun_buku` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- ----------------------------
--- Records of buku
--- ----------------------------
+-- --------------------------------------------------------
 
--- ----------------------------
--- Table structure for peminjaman
--- ----------------------------
-DROP TABLE IF EXISTS `peminjaman`;
+--
+-- Struktur dari tabel `peminjaman`
+--
+
 CREATE TABLE `peminjaman` (
-  `id_peminjaman` varchar(20) NOT NULL,
+  `id_peminjaman` int(20) NOT NULL,
   `tgl_pinjam` date DEFAULT NULL,
   `tgl_kembali` date DEFAULT NULL,
-  `id_anggota` varchar(20) DEFAULT NULL,
-  `id_buku` varchar(20) DEFAULT NULL,
-  `no_petugas` varchar(20) DEFAULT NULL,
-  PRIMARY KEY (`id_peminjaman`)
+  `id_anggota` int(20) DEFAULT NULL,
+  `id_buku` int(20) DEFAULT NULL,
+  `no_petugas` int(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- ----------------------------
--- Records of peminjaman
--- ----------------------------
+-- --------------------------------------------------------
 
--- ----------------------------
--- Table structure for petugas
--- ----------------------------
-DROP TABLE IF EXISTS `petugas`;
+--
+-- Struktur dari tabel `petugas`
+--
+
 CREATE TABLE `petugas` (
-  `no_petugas` int(20) NOT NULL AUTO_INCREMENT,
+  `no_petugas` int(20) NOT NULL,
   `nama` char(20) NOT NULL,
   `jabatan` varchar(20) NOT NULL,
   `username` varchar(20) NOT NULL,
-  `password` varchar(20) NOT NULL,
-  PRIMARY KEY (`no_petugas`)
+  `password` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- ----------------------------
--- Records of petugas
--- ----------------------------
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `anggota`
+--
+ALTER TABLE `anggota`
+  ADD PRIMARY KEY (`id_anggota`);
+
+--
+-- Indeks untuk tabel `buku`
+--
+ALTER TABLE `buku`
+  ADD PRIMARY KEY (`id_buku`);
+
+--
+-- Indeks untuk tabel `peminjaman`
+--
+ALTER TABLE `peminjaman`
+  ADD PRIMARY KEY (`id_peminjaman`),
+  ADD KEY `id_anggota` (`id_anggota`),
+  ADD KEY `id_buku` (`id_buku`),
+  ADD KEY `no_petugas` (`no_petugas`);
+
+--
+-- Indeks untuk tabel `petugas`
+--
+ALTER TABLE `petugas`
+  ADD PRIMARY KEY (`no_petugas`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `buku`
+--
+ALTER TABLE `buku`
+  MODIFY `id_buku` int(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `peminjaman`
+--
+ALTER TABLE `peminjaman`
+  MODIFY `id_peminjaman` int(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `petugas`
+--
+ALTER TABLE `petugas`
+  MODIFY `no_petugas` int(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `peminjaman`
+--
+ALTER TABLE `peminjaman`
+  ADD CONSTRAINT `peminjaman_ibfk_1` FOREIGN KEY (`id_anggota`) REFERENCES `anggota` (`id_anggota`),
+  ADD CONSTRAINT `peminjaman_ibfk_2` FOREIGN KEY (`id_buku`) REFERENCES `buku` (`id_buku`),
+  ADD CONSTRAINT `peminjaman_ibfk_3` FOREIGN KEY (`no_petugas`) REFERENCES `petugas` (`no_petugas`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
